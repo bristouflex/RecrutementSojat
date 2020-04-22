09090eb369b4cce304adbbd9b6d0a234bea7c7d6
Alexis Delée <alexisdelee@protonmail.com>
2020-04-22 15:42:25 +0200

    et si on faisait des tests...

--- src/Créneau.ts
+++ src/Créneau.ts
@@ -1,22 +1,25 @@
 export default class Créneau {
-    private date: Date;
-    private heureDébut: number;
-    private heureFin: number;
+    public readonly date: Date;
+    public readonly heureDébut: number;
+    public readonly heureFin: number;
 
     /**
      * @param dateTime 
      * @param durée en minute
      */
     constructor(dateTime: Date, durée: number) {
+        if (durée <= 10) {
+            throw new Error("Une réunion ne peut être inférieure à 10 minutes");
+        } else if (durée > 8 * 60) {
+            throw new Error("Une réunion est limitée à 8h");
+        }
+
         this.date = new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate());
         this.heureDébut = dateTime.getTime() - this.date.getTime();
         this.heureFin = this.heureDébut + durée * 60_000;
-
-        console.log(new Date(this.date.getTime() + this.heureDébut));
-        console.log(new Date(this.date.getTime() + this.heureFin));
     }
 
-    public equals(date: Date): boolean {
-        return this.date.getTime() === date.getTime();
+    public equals(créneau: Créneau): boolean {
+        return this.date.getTime() === créneau.date.getTime();
     }
 }
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import Créneau from "./Créneau";
-
-new Créneau(new Date(), 120);
