e739ab941f95ec2e4d0f5d8ff8507f222bd787b5
Bristouflex <b.hounza@laposte.net>
2020-04-24 14:18:31 +0200

    go

--- src/Entretien.spec.ts
+++ src/Entretien.spec.ts
@@ -12,6 +12,13 @@ describe("test sur les entretiens", () => {
         expect(entretien.status).toEqual(Status.Confirme)
     })
 
+    test("étant donné un entretien au status planifie, il doit retourner une erreur si le specialiste technique ne correspond pas niveau compétences", () => {
+        let createErrorEntretien = () => {
+            let entretien = new Entretien(Status.Plannifie, new Creneau(new Date(2020, 1, 1, 18), 60), new SpecialisteTech("Alexis", "Delee", [Competence.DotNet]), new Candidat("Bob", "Dylan", new CV(new Blob())), Competence.JS)
+        }
+        expect(createErrorEntretien).toThrow(Error);
+    })
+
 
 })
 
