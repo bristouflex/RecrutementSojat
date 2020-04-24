b3ddf1c8393547f5c4e12c7d8da900799b9eae91
NealKemissi <neal.k@hotmail.fr>
2020-04-24 12:38:18 +0200

    modif repository specialistTech

--- src/repository/SpecialisteTechRepository.ts
+++ src/repository/SpecialisteTechRepository.ts
@@ -1,24 +1,63 @@
-import SpecialisteTech from "model/specialisteTech/SpecialisteTech";
+import SpecialisteTech, {
+  Competence,
+} from "model/specialisteTech/SpecialisteTech";
 import Creneau from "Creneau";
 
 export default class SpecialisteTechRepository {
+  /**
+   * Retourne tout les recruteurs technique
+   * @param crenea
+   * @returns SpecialisteTech[]
+   */
   public findAll(): SpecialisteTech[] | undefined {
     return;
   }
 
+  /**
+   * Retourne un recruteur en fonction de son id
+   * @param id
+   * @returns SpecialisteTech
+   */
   public finById(id: number): SpecialisteTech | undefined {
     return;
   }
 
-  public findAllAvailable(creneau: Creneau): SpecialisteTech | undefined {
+  /**
+   * Retourne tout les recruteurs technique disponnible
+   * pour un créneau donnée
+   * @param crenea
+   * @returns SpecialisteTech[]
+   */
+  public findAllAvailable(creneau: Creneau): SpecialisteTech[] | undefined {
     return;
   }
 
-  public update(): void {
+  /**
+   * Retourne tout les recruteurs technique disponnible
+   * pour un créneau donnée et des compétences requises
+   * @param crenea
+   * @returns SpecialisteTech[]
+   */
+  public findAvailableByCompetences(
+    creneau: Creneau,
+    competences: Competence[]
+  ): SpecialisteTech[] | undefined {
     return;
   }
 
-  public delete(): void {
+  /**
+   * Met à jour un recruteur
+   * @param specialisteTech
+   */
+  public update(specialisteTech: SpecialisteTech): void {
+    return;
+  }
+
+  /**
+   * Suppression d'un recruteur
+   * @param id
+   */
+  public deleteById(id: number): void {
     return;
   }
 }
